27ab24c4edbcf0f7dcd1297d64bcb997a59ff4ab
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2017-02-07 21:39:46 -0500

    Added tests for renderPlaceholder

--- tests/HypernovaTest.php
+++ tests/HypernovaTest.php
@@ -36,4 +36,35 @@ class HypernovaTest extends TestCase
         $uuid = $this->hypernova->addJob($job);
         $this->assertEquals($job, $this->hypernova->getJob($uuid));
     }
+
+    /**
+     * Test rendering placeholder
+     *
+     * @test
+     * @covers ::renderPlaceholder
+     */
+    public function testRenderPlaceholder()
+    {
+        $job = [
+            'name' => 'MyComponent',
+            'data' => [
+                'test' => 1
+            ]
+        ];
+
+        $uuid = $this->hypernova->addJob($job['name'], $job['data']);
+        $placeholder = $this->hypernova->renderPlaceholder($uuid);
+        $document = new \DOMDocument();
+        $document->loadHTML($placeholder);
+        $div = $document->documentElement->getElementsByTagName('div')[0];
+        $this->assertEquals($job['name'], $div->getAttribute('data-hypernova-key'));
+        $this->assertEquals($uuid, $div->getAttribute('data-hypernova-id'));
+
+        $script = $document->documentElement->getElementsByTagName('script')[0];
+        $this->assertEquals($job['name'], $script->getAttribute('data-hypernova-key'));
+        $this->assertEquals($uuid, $script->getAttribute('data-hypernova-id'));
+        $json = preg_replace('/^\<\!\-\-/', '', preg_replace('/\-\-\>$/', '', $script->textContent));
+        $data = json_decode($json, true);
+        $this->assertEquals($job['data'], $data);
+    }
 }
